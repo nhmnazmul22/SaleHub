8f4a91c9b8430500a5f3cd1ebef6eb65814b91ea
nhmnazmul22 <najmulmonir12345@gmail.com>
2026-01-31 22:00:40 +0600

    app sidebar done with userNavbar

--- src/components/Layouts/AppSidebar/AppSidebar.tsx
+++ src/components/Layouts/AppSidebar/AppSidebar.tsx
@@ -1,41 +1,45 @@
 import {
-    Sidebar,
-    SidebarContent,
-    SidebarFooter,
-    SidebarHeader,
-    SidebarMenu,
-    SidebarMenuButton,
-    SidebarMenuItem,
-} from "@/components/ui/sidebar"
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
 import Logo from "@/components/common/Logo/Logo";
-import { Separator } from "@/components/ui/separator"
-import {menuItems} from "@/constants";
+import { Separator } from "@/components/ui/separator";
+import { menuItems } from "@/constants";
 import Link from "next/link";
+import { NavUser } from "./NavUser";
 const AppSidebar = () => {
-    return (
-        <Sidebar>
-            <SidebarHeader className="p-5">
-                <Logo/>
-            </SidebarHeader>
-            <Separator/>
-            <SidebarContent >
-                <SidebarMenu className="mt-5">
-                    {menuItems.map((item) => (
-                        <SidebarMenuItem key={item.id}>
-                            <SidebarMenuButton  asChild>
-                                <Link className="p-5"   href={item.id}>
-                                    <item.icon />
-                                    <span>{item.label}</span>
-                                </Link>
-                            </SidebarMenuButton>
-                        </SidebarMenuItem>
-                    ))}
-                </SidebarMenu>
-
-            </SidebarContent>
-            <SidebarFooter />
-        </Sidebar>
-    );
+  return (
+    <Sidebar>
+      <SidebarHeader className="p-5">
+        <Logo />
+      </SidebarHeader>
+      <Separator />
+      <SidebarContent>
+        <SidebarMenu className="mt-5">
+          {menuItems.map((item) => (
+            <SidebarMenuItem key={item.id}>
+              <SidebarMenuButton asChild>
+                <Link className="p-5" href={item.id}>
+                  <item.icon />
+                  <span>{item.label}</span>
+                </Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          ))}
+        </SidebarMenu>
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser
+          user={{ name: "Jhon Deo", email: "name@example.com", avatar: "" }}
+        />
+      </SidebarFooter>
+    </Sidebar>
+  );
 };
 
 export default AppSidebar;
---
+++ src/components/Layouts/AppSidebar/NavUser.tsx
@@ -0,0 +1,93 @@
+"use client";
+
+import {Avatar, AvatarFallback, AvatarImage} from "@/components/ui/avatar";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuGroup,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {SidebarMenu, SidebarMenuButton, SidebarMenuItem, useSidebar,} from "@/components/ui/sidebar";
+import {Bell, LogOut, Settings, UserCircle} from "lucide-react";
+
+export function NavUser({
+  user,
+}: {
+  user: {
+    name: string;
+    email: string;
+    avatar: string;
+  };
+}) {
+  const { isMobile } = useSidebar();
+
+  return (
+    <SidebarMenu>
+      <SidebarMenuItem>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <SidebarMenuButton
+              size="lg"
+              className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+            >
+              <Avatar className="h-8 w-8 rounded-lg grayscale">
+                <AvatarImage src={user.avatar} alt={user.name} />
+                <AvatarFallback className="rounded-lg">CN</AvatarFallback>
+              </Avatar>
+              <div className="grid flex-1 text-left text-sm leading-tight">
+                <span className="truncate font-medium">{user.name}</span>
+                <span className="text-muted-foreground truncate text-xs">
+                  {user.email}
+                </span>
+              </div>
+            </SidebarMenuButton>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent
+            className="w-(--radix-dropdown-menu-trigger-width) min-w-56 rounded-lg"
+            side={isMobile ? "bottom" : "right"}
+            align="end"
+            sideOffset={4}
+          >
+            <DropdownMenuLabel className="p-0 font-normal">
+              <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+                <Avatar className="h-8 w-8 rounded-lg">
+                  <AvatarImage src={user.avatar} alt={user.name} />
+                  <AvatarFallback className="rounded-lg">CN</AvatarFallback>
+                </Avatar>
+                <div className="grid flex-1 text-left text-sm leading-tight">
+                  <span className="truncate font-medium">{user.name}</span>
+                  <span className="text-muted-foreground truncate text-xs">
+                    {user.email}
+                  </span>
+                </div>
+              </div>
+            </DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuGroup>
+              <DropdownMenuItem className="group">
+                <UserCircle className="group-hover:text-white"/>
+                Account
+              </DropdownMenuItem>
+              <DropdownMenuItem className="group">
+                <Bell className="group-hover:text-white"/>
+                Notifications
+              </DropdownMenuItem>
+                <DropdownMenuItem>
+                    <Settings className="group-hover:text-white" />
+                    Settings
+                </DropdownMenuItem>
+            </DropdownMenuGroup>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem variant="destructive" className="group">
+              <LogOut/>
+              Log out
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </SidebarMenuItem>
+    </SidebarMenu>
+  );
+}
