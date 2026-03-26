7304888ac0b4864cb688fe0f2046f8a665e6b9f3
nhmnazmul22 <najmulmonir12345@gmail.com>
2026-03-26 22:30:26 +0600

    Add `Unit` factory and seeder to support measurement unit data population.

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -16,7 +16,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             BranchSeeder::class,
-            CategorySeeder::class
+            CategorySeeder::class,
+            UnitSeeder::class,
         ]);
     }
 }
