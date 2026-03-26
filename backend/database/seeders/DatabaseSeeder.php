9dbebca95ef3db30241d3f6fb55e2efb5e823a01
nhmnazmul22 <najmulmonir12345@gmail.com>
2026-03-26 22:18:08 +0600

    Add `Category` seeder and factory to populate sample hierarchical categories.

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,8 +14,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-       $this->call([
-          BranchSeeder::class
-       ]);
+        $this->call([
+            BranchSeeder::class,
+            CategorySeeder::class
+        ]);
     }
 }
