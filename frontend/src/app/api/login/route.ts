e994a045b41a47300f1467c470f98237b135d5c7
nhmnazmul22 <najmulmonir12345@gmail.com>
2026-03-29 23:05:58 +0600

    feat: implement user login functionality with JWT authentication and password validation

--- frontend/src/app/api/login/route.ts
+++ frontend/src/app/api/login/route.ts
@@ -1,49 +1,88 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import { NextRequest, NextResponse } from "next/server";
-import { registrationValidation } from "@/features/auth/shared/auth.validation";
+import { loginValidation } from "@/features/auth/shared/auth.validation";
 import { z } from "zod";
 import UserModel from "@/features/user/server/user.model";
-import { RoleEnumType } from "@/types/enumType";
+import { connectDB } from "@/config/database";
 
 export async function POST(req: NextRequest) {
   try {
+    // Connect the DB
+    await connectDB();
+
     const body = await req.json();
-    const validationResult = registrationValidation.safeParse(body);
+
+    // Validate the request body
+    const validationResult = loginValidation.safeParse(body);
     if (!validationResult.success) {
       return NextResponse.json(
         {
           success: false,
+          message: "Validation failed",
           errors: z.flattenError(validationResult.error).fieldErrors,
         },
         { status: 400 },
       );
     }
 
-    const { email } = validationResult.data;
+    const { email, password } = validationResult.data;
+    // Check if the user already exists or not
     const existUser = await UserModel.findOne({ email });
-    if (existUser) {
+    if (!existUser) {
       return NextResponse.json(
         {
           success: false,
-          message: "User already exists",
+          message: "User not found",
         },
-        { status: 400 },
+        { status: 404 },
       );
     }
 
-    const newUser = await UserModel.create({
-      ...validationResult.data,
-      role: RoleEnumType.ADMIN,
+    // Check the password is correct or not
+    const isPasswordValid = await bcrypt.compare(password, existUser.password);
+    if (!isPasswordValid) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: "Invalid password",
+        },
+        { status: 401 },
+      );
+    }
+
+    // Generate a Token
+    const token = jwt.sign(
+      {
+        id: existUser._id,
+        role: existUser.role,
+        isActive: existUser.isActive,
+      },
+      process.env.JWT_SECRET_KEY!,
+      { expiresIn: Number(process.env.JWT_EXPIRES_IN) },
+    );
+
+    const response = NextResponse.json({
+      success: true,
+      message: "User logged in successfully",
+      token: token,
     });
 
-    return NextResponse.json({
-      success: true,
-      message: "User registered successfully",
-      data: newUser,
+    response.cookies.set("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "strict",
+      maxAge: Number(process.env.JWT_EXPIRES_IN),
     });
+
+    return response;
   } catch (error) {
-    console.log(error);
     return NextResponse.json(
-      { success: false, message: "Internal server error" },
+      {
+        success: false,
+        message:
+          error instanceof Error ? error.message : "Internal server error",
+      },
       { status: 500 },
     );
   }
