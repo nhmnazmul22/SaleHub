be9ea2f7f3732761f803a44101621e08e72e5ebb
nhmnazmul22 <najmulmonir12345@gmail.com>
2026-04-10 22:09:58 +0600

    Refactor user validation schema for improved readability and maintainability

--- src/features/user/shared/user.validation.ts
+++ src/features/user/shared/user.validation.ts
@@ -2,20 +2,31 @@ import { objectId } from "@/types";
 import { z } from "zod";
 import { RoleEnumType } from "@/types/enumType";
 
-export const userSchemaType = z.object({
-  firstName: z.string().trim().min(1, "First name is required"),
-  lastName: z.string().trim().optional(),
-  email: z.email().trim(),
-  password: z
-    .string()
-    .regex(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/),
-  role: z.enum(Object.values(RoleEnumType), "Invalid Role"),
-  branchId: objectId,
-  isActive: z.boolean().default(true),
-  lastLogin: z.date().nullable(),
-  createdAt: z.date().optional(),
-  updatedAt: z.date().optional(),
-  deletedAt: z.date().optional(),
-});
+export const userSchemaType = z
+  .object({
+    firstName: z.string().trim().min(1, "First name is required"),
+    lastName: z.string().trim().optional(),
+    email: z.email().trim(),
+    password: z
+      .string()
+      .regex(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/),
+    role: z.enum(Object.values(RoleEnumType), "Invalid Role"),
+    branchId: objectId,
+    isActive: z.boolean().default(true),
+    lastLogin: z.date().nullable(),
+    createdAt: z.date().optional(),
+    updatedAt: z.date().optional(),
+    deletedAt: z.date().optional(),
+  })
+  .superRefine((data, ctx) => {
+    if (data.role !== "admin" && !data.branchId) {
+      ctx.addIssue({
+        code: "custom",
+        message: "Branch ID is required for non-admin users",
+        input: data.branchId,
+        path: ["branchId"],
+      });
+    }
+  });
 
 export type UserType = z.infer<typeof userSchemaType>;
