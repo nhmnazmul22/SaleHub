ba4f37f8107a7249b72af1815395251bf90a00c2
nhmnazmul22 <najmulmonir12345@gmail.com>
2026-04-10 22:58:18 +0600

    Implement branch creation endpoint with validation and error handling

--- src/app/api/branches/route.ts
+++ src/app/api/branches/route.ts
@@ -1,7 +1,9 @@
 import { connectDB } from "@/config/database";
 import ResponseStatus from "@/config/status";
 import BranchModel from "@/features/branch/server/branch.model";
+import { branchSchemaType } from "@/features/branch/shared/branch.validation";
 import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
 
 /**
  * Get all branch list
@@ -27,3 +29,61 @@ export async function GET() {
     );
   }
 }
+
+/**
+ * Create new branch
+ * @params req NextRequest
+ */
+export async function POST(req: NextRequest) {
+  try {
+    await connectDB();
+    const body = await req.json();
+
+    const validationResult = branchSchemaType.safeParse(body);
+
+    if (!validationResult.success) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: "Invalid branch data",
+          errors: z.flattenError(validationResult.error).fieldErrors,
+        },
+        { status: ResponseStatus.UNPROCESSABLE_ENTITY },
+      );
+    }
+
+    const branchInfo = validationResult.data;
+    // Checking branch exist or not
+    const existBranch = await BranchModel.findOne({ name: branchInfo.name });
+    if (existBranch) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: `${branchInfo.name} already exist. Try with another name.`,
+        },
+        { status: ResponseStatus.BAD_REQUEST },
+      );
+    }
+
+    const newBranch = await BranchModel.create(branchInfo);
+
+    return NextResponse.json(
+      {
+        success: true,
+        message: "Branch create successful",
+        data: newBranch,
+      },
+      { status: ResponseStatus.CREATED },
+    );
+  } catch (error) {
+    return NextResponse.json(
+      {
+        success: false,
+        message: "An error occurred while create new branch",
+        error: error instanceof Error ? error.message : "Unknown error",
+      },
+      { status: ResponseStatus.INTERNAL_SERVER_ERROR },
+    );
+  }
+}
+
