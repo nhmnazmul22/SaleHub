38c38e1ea9b6274299bb37af5bd7542839b7057d
nhmnazmul22 <najmulmonir12345@gmail.com>
2026-04-14 12:54:19 +0600

    Introduce vitest for integration and unit

---
+++ vitest.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "vitest/config";
+import path from "path";
+
+export default defineConfig({
+  test: {
+    environment: "node",
+    globals: true,
+    setupFiles: ["./vitest.setup.ts"],
+    coverage: {
+      provider: "v8",
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
