7cfd3b1ca38bc5b7a6b62c155fd49960f001e4ab
nhmnazmul22 <najmulmonir12345@gmail.com>
2026-04-14 16:02:00 +0600

    update database config for resolve testing error

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,13 +1,5 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI;
-
-if (!MONGODB_URI) {
-  throw new Error(
-    "Please define the MONGODB_URI environment variable inside .env.local",
-  );
-}
-
 type MongooseCache = {
   conn: typeof mongoose | null;
   promise: Promise<typeof mongoose> | null;
@@ -21,7 +13,17 @@ const cached =
   global.mongooseCache ??
   (global.mongooseCache = { conn: null, promise: null });
 
-export async function connectDB(): Promise<typeof mongoose> {
+export async function connectDB(): Promise<typeof mongoose | undefined> {
+  if (mongoose.connection.readyState >= 1) return;
+
+  const MONGODB_URI = process.env.MONGODB_URI;
+
+  if (!MONGODB_URI) {
+    throw new Error(
+      "Please define the MONGODB_URI environment variable inside .env.local",
+    );
+  }
+
   if (cached.conn) {
     return cached.conn;
   }
